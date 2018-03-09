9438257c5ee6b37a9da0a5ff6c2edf5a34fc3d7d
HASSAN ARSHAD <hassanarshad115@gmail.com>
2018-03-09 13:49:10 -0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_CRUD_Operations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = GetData();
+
+        }
+
+
+        private DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+            string connectionstring = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from itbl", conn);
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        private void addNewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //add new record pr click kro tw ak method call hoy
+            methodInFirstForm(0, false);
+        }
+
+        //jo method uper declare kia ha uski coding
+        private void methodInFirstForm(int idV, bool UpdateV)
+        {
+            //insert wala form open hoy r usmy jo 2 properties bnae ti unhy asy access krna ha
+            Form2 obj = new Form2();
+            obj.idP = idV;
+            obj.isUpdateP = UpdateV;
+            obj.Show();
+        }
+
+        //datagridview pr double click kr k jo event hoga..
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            int index = dataGridView1.Rows.GetFirstRow(DataGridViewElementStates.Selected);
+            int updateObj = (int)dataGridView1.Rows[index].Cells[0].Value;
+            //jo method upser add new record ko jb click krty ha usmy bnaya ta usko yha agian call ki but obj r true pas krdia
+            methodInFirstForm(updateObj, true);
+            //bs datagridview ma jst ye 3 lines he likhni  ha..
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_CRUD_Operations
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        //insert waly ma properties bnaygy na k gridview waly ma
+        public int idP { get; set; }
+        public bool isUpdateP { get; set; }
+
+        //isUpdateP k ander update ka method he call krana ha else ma save krna ha yad rkhna
+        //insert ma jo save button ha ye code uska ha
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isUpdateP)
+            {
+                UpdateRecordMethod();
+            }
+            else
+            {
+                SaveRecordMethod();
+            }
+
+        }
+        //whe porana treeka save r update wala
+        private void SaveRecordMethod()
+        {
+            string connectionstring = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand cmd = new SqlCommand("i", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@n", textBox1.Text);
+            cmd.Parameters.AddWithValue("@fn", textBox2.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Test successfully");
+        }
+
+        private void UpdateRecordMethod()
+        {
+            string connectionstring = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand cmd = new SqlCommand("up", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@n", textBox1.Text);
+            cmd.Parameters.AddWithValue("@fn", textBox2.Text);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Test successfully");
+        }
+
+
+        //ye nichy waly 2 kam insert form k lod event ma hongy
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            if (isUpdateP)
+            {
+                DataTable dt = GetDataByIDMethod(idP);//ak method bnaygy ismy id property pass krygy
+                DataRow dr = dt.Rows[0]; //datarow ka obj bnaygy jsko datatabke ka obj with Rows[0] k sath jor dyngy
+
+                //jtny b textbox ka combobox ya images honge unko nichy asy likhna ha
+                textBox1.Text = dr["name"].ToString();
+                textBox2.Text = dr["fatherName"].ToString();
+            }
+        }
+
+        private DataTable GetDataByIDMethod(int idPMv)
+        {
+            //sbsy phly datatale ka obj bnana ha jo k last m return b hoga
+            DataTable dt = new DataTable();
+
+            string connectionstring = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionstring);
+            SqlCommand cmd = new SqlCommand("IdAccess", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("id", idPMv);
+            conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            dt.Load(reader);
+
+            conn.Close();
+
+            return dt;
+        }
+    }
+}
